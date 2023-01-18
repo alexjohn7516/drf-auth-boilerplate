983cac637267b94ab2bd7f0b27797e340d31f537
Alexander Castillo <jhuiicebox@gmail.com>
2023-01-17 18:20:32 -0700

    initial commit

---
+++ backend/mockapi/urls.py
@@ -0,0 +1,38 @@
+"""mockapi URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView
+)
+from mockAPI.views import GoogleLogin
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('auth/', include('dj_rest_auth.urls')),
+    path('auth/registration/', include('dj_rest_auth.registration.urls')),
+    # path('auth/google', )
+    # simple jwt token creation and refreshing
+    path('api/token/', TokenObtainPairView.as_view()),
+    path('api/token/refresh/', TokenRefreshView.as_view()),
+
+    # http://localhost:8000/accounts/google/loggin sends you to login page for django
+    # ? Can i a get a token from this endpoint?
+    # ? Where is the data flowing?
+    path('accounts/', include('allauth.urls')),
+    path('auth/google', GoogleLogin.as_view(), name='google_login'),
+]
